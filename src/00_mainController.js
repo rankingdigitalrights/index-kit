5b676d4f1d2f7d2c83b0c5882d2e4a3fd9b6914a
gabywalton <52406480+gabywalton@users.noreply.github.com>
2021-04-27 10:00:57 -0400

    Update 00_mainController.js

--- src/00_mainController.js
+++ src/00_mainController.js
@@ -1,15 +1,15 @@
 /* this function creates input sheets for specified companies in the designated
  output folder by calling createSpreadsheetInput() and adds the sheets to the control sheet by calling addFileIDtoControl()*/
 
- //"use strict"
+//"use strict"
 
 function mainInputSheets() {
-  initiateGlobalConfig();
-  filenameSuffix = "Dev Index Kit"; // Dev, "", Debug, QC
-  let mainSheetMode = "Input"; // for filename
-  let useIndicatorSubset = true; // true := use subset
+  initiateGlobalConfig()
+  filenameSuffix = 'Dev Index Kit' // Dev, "", Debug, QC
+  let mainSheetMode = 'Input' // for filename
+  let useIndicatorSubset = true // true := use subset
 
-  let Companies = companiesVector.companies.slice(0, 0); // on purpose to prevent script from running.
+  let Companies = companiesVector.companies.slice(0, 0) // on purpose to prevent script from running.
   // .slice(0,3) // Subset #1 0:2
   // .slice(3,6) // Subset #2 3:5
   // .slice(6,9) // Subset #3 6:8
@@ -24,7 +24,7 @@ function mainInputSheets() {
   // .slice(7,8) // Twitter
   // .slice(8,9) // Vodafone
 
-  let fileID;
+  let fileID
 
   Companies.forEach((Company) => {
     fileID = createSpreadsheetInput({
@@ -33,38 +33,30 @@ function mainInputSheets() {
       filenamePrefix,
       filenameSuffix,
       mainSheetMode,
-    });
+    })
 
-    addFileIDtoControl(
-      mainSheetMode,
-      Company.label.current,
-      fileID,
-      controlSpreadsheetID
-    );
-  });
+    addFileIDtoControl(mainSheetMode, Company.label.current, fileID, controlSpreadsheetID)
+  })
 }
 
 function initiateGlobalConfig() {
-  indexPrefix = centralConfig.indexPrefix;
-  filenamePrefix = "2019 Pilot -";
-  filenameSuffix = "Dev"; // Dev, "", Debug, QC
-  rootFolderID = centralConfig.rootFolderID; // "2019 Back-End Dev"
-  outputFolderName = "2019 Pilot Demo"; // "2019 Pilot Data Store"
+  indexPrefix = centralConfig.indexPrefix
+  filenamePrefix = '2021 Mini Index -'
+  filenameSuffix = 'Dev' // Dev, "", Debug, QC
+  rootFolderID = centralConfig.rootFolderID // "2019 Back-End Dev"
+  outputFolderName = '2021 Mini Index' // "2019 Pilot Data Store"
 
-  controlSpreadsheetID = centralConfig.controlSpreadsheetID; // 00_2019_Pilot_Dashboard
+  controlSpreadsheetID = centralConfig.controlSpreadsheetID // 00_2019_Pilot_Dashboard
 }
 
-
 function mainScoringSheets() {
-
   initiateGlobalConfig()
-  outputFolderName = "Index Kit Scores Dev"
-  var mainSheetMode = "Output"
+  outputFolderName = 'Index Kit Scores Dev'
+  var mainSheetMode = 'Output'
   var useStepsSubset = false // true := use subset
   var useIndicatorSubset = false // true := use subset
 
-  var Companies = companiesVector.companies
-      .slice(1, 9)
+  var Companies = companiesVector.companies.slice(1, 9)
   // .slice(0,1) // Amazon
   // .slice(1, 2) // Apple
   // .slice(3,4) //
@@ -72,11 +64,15 @@ function mainScoringSheets() {
   var fileID
 
   Companies.forEach(function (Company) {
+    fileID = createSpreadsheetOutput(
+      useStepsSubset,
+      useIndicatorSubset,
+      Company,
+      filenamePrefix,
+      filenameSuffix,
+      mainSheetMode
+    )
 
-      fileID = createSpreadsheetOutput(useStepsSubset, useIndicatorSubset, Company, filenamePrefix, filenameSuffix, mainSheetMode)
-
-      addFileIDtoControl(mainSheetMode, Company.label.current, fileID, controlSpreadsheetID)
-
+    addFileIDtoControl(mainSheetMode, Company.label.current, fileID, controlSpreadsheetID)
   })
 }
-
